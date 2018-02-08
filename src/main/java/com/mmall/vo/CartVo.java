538a4fddb3ad629adfdc67def56944a7bccc0cbc
zhangqc <zhangqingcai0815@gmail.com>
2018-02-08 20:18:33 +0800

    添加购物车、更新购物车

---
+++ src/main/java/com/mmall/vo/CartVo.java
@@ -0,0 +1,47 @@
+package com.mmall.vo;
+
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class CartVo {
+
+    List<CartProductVo> cartProductVoList;
+    BigDecimal cartTotalPrice;
+    private Boolean allChecked;
+    private String imageHost;
+
+
+    public List<CartProductVo> getCartProductVoList() {
+        return cartProductVoList;
+    }
+
+    public void setCartProductVoList(List<CartProductVo> cartProductVoList) {
+        this.cartProductVoList = cartProductVoList;
+    }
+
+    public BigDecimal getCartTotalPrice() {
+        return cartTotalPrice;
+    }
+
+    public void setCartTotalPrice(BigDecimal cartTotalPrice) {
+        this.cartTotalPrice = cartTotalPrice;
+    }
+
+    public Boolean getAllChecked() {
+        return allChecked;
+    }
+
+    public void setAllChecked(Boolean allChecked) {
+        this.allChecked = allChecked;
+    }
+
+    public String getImageHost() {
+        return imageHost;
+    }
+
+    public void setImageHost(String imageHost) {
+        this.imageHost = imageHost;
+    }
+}
