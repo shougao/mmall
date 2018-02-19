3db3c734e148c64f2b0d5012ed00eca6f0c1b581
zhangqc <zhangqingcai0815@gmail.com>
2018-02-19 23:18:14 +0800

    三种文件下载方式via HttpServletResponse， via HttpEntity， via Resource

---
+++ src/main/java/com/mmall/controller/portal/DownloadController.java
@@ -0,0 +1,71 @@
+package com.mmall.controller.portal;
+
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+
+@Controller
+@RequestMapping("/download")
+public class DownloadController {
+
+    private static final String FILE_PATH = "/Users/eric/Downloads/pic.jpg";
+    private static final String APPLICATION_JPG = "application/jpg";
+
+
+//    via HttpServletResponse
+    @RequestMapping(value = "/a.do", method = RequestMethod.GET, produces = APPLICATION_JPG)
+    public @ResponseBody
+    void downloadA(HttpServletResponse response) throws IOException {
+        File file = getFile();
+        InputStream in = new FileInputStream(file);
+
+        response.setContentType(APPLICATION_JPG);
+        response.setHeader("Content-Disposition", "attachment; filename=" + file.getName());
+        response.setHeader("Content-Length", String.valueOf(file.length()));
+        FileCopyUtils.copy(in, response.getOutputStream());
+    }
+
+//    via HttpEntity
+    @RequestMapping(value = "/b.do", method = RequestMethod.GET, produces = APPLICATION_JPG)
+    public @ResponseBody
+    HttpEntity<byte[]> downloadB() throws IOException {
+        File file = getFile();
+        byte[] document = FileCopyUtils.copyToByteArray(file);
+
+        HttpHeaders header = new HttpHeaders();
+        header.setContentType(new MediaType("application", "jpg"));
+        header.set("Content-Disposition", "inline; filename=" + file.getName());
+        header.setContentLength(document.length);
+
+        return new HttpEntity<byte[]>(document, header);
+    }
+
+    @RequestMapping(value = "/c.do", method = RequestMethod.GET, produces = APPLICATION_JPG)
+    public @ResponseBody
+    Resource downloadC(HttpServletResponse response) throws FileNotFoundException {
+        File file = getFile();
+        response.setContentType(APPLICATION_JPG);
+        response.setHeader("Content-Disposition", "inline; filename=" + file.getName());
+        response.setHeader("Content-Length", String.valueOf(file.length()));
+        return new FileSystemResource(file);
+    }
+
+//    via Resource
+    private File getFile() throws FileNotFoundException {
+        File file = new File(FILE_PATH);
+        if (!file.exists()){
+            throw new FileNotFoundException("file with path: " + FILE_PATH + " was not found.");
+        }
+        return file;
+    }
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="c" uri="http://www.springframework.org/tags" %>
 <html>
 <body>
 <h2>z!</h2>
@@ -12,6 +13,18 @@ springmvc richtext upload
 <form name="form2" action="/manage/product/richtext_img_upload.do" method="post" enctype="multipart/form-data">
     <input type="file" name="uploadfile">
     <input type="submit" name="springmvc richtext file upload file">
+</form>
+
+
+<form>
+    <h1>Spring MVC Download File Example</h1>
+
+    <a href="<c:url value='/download/a.do'/>">Download file via servlet response.</a><br/>
+    <form name="download" action="/download/a.do" method="post">
+
+    </form>
+    <a href="<c:url value='/download/b.do'/>">Download file via http entity.</a><br/>
+    <a href="<c:url value='/download/c.do'/>">Download file via file system resource.</a><br/>
 </form>
 
 </body>
